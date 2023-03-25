657627d6b9a7ec5d51498a0b748f18df34c49d20
Folashade Obada <folacrownie@gmail.com>
2023-03-25 08:04:55 -0700

    updates

---
+++ main.h
@@ -0,0 +1,27 @@
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fat_t - struct op
+ "main.h" 117L 3520C 
+"main.h" 35L, 511C
